a305ca55c02b39ccd7445ddab62e2956c70fe121
dongyuekai <457545648@qq.com>
2023-09-12 14:02:01 +0800

    feat: 用装饰器setMetadata

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaGuard } from './aaa.guard';
+import { Aaa } from './aaa.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  // @SetMetadata('aaa', 'admin') // 不同的metadata设置太原始了 可以通过装饰器来设置
+  @Aaa('admin2')
+  @UseGuards(AaaGuard)
   getHello(): string {
     return this.appService.getHello();
   }
